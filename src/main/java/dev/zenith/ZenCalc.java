ff0d6fe4cb5d9e982b0c74d388830c4e8f1e6952
Zenith <cenith@tutamail.com>
2026-01-11 13:04:07 +0545

    Correcting an oopise...

---
+++ src/main/java/dev/zenith/ZenCalc.java
@@ -0,0 +1,53 @@
+package dev.zenith;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "ZenCalc", mixinStandardHelpOptions = true, version = "1.0", description = "A lvl 0 calculator for the Zen series.")
+
+public class ZenCalc implements Callable<Integer> {
+
+  @Option(names = { "-a", "--alpha" }, description = "First number", required = true)
+  private double a;
+
+  @Option(names = { "-b", "--beta" }, description = "Second number", required = true)
+  private double b;
+
+  @Option(names = { "-op", "--operation" }, description = "add, sub, mul, div", required = true)
+  private String op;
+
+  @Override
+  public Integer call() throws Exception {
+
+    double result = 0;
+
+    try {
+      result = switch (op.toLowerCase()) {
+        case "add" -> a + b;
+        case "sub" -> a - b;
+        case "mul" -> a * b;
+        case "div" -> {
+          if (b == 0)
+            throw new ArithmeticException("Cannot divide by Zero.");
+          yield a / b;
+        }
+        default -> throw new IllegalArgumentException("Unknown operation " + op);
+      };
+
+      System.out.printf("ZenCalc Result: %.2f%n", result);
+      return 0;
+
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+      return 1;
+    }
+  }
+
+  public static void main(String[] args) {
+    int exitCode = new CommandLine(new ZenCalc()).execute(args);
+    System.exit(exitCode);
+  }
+}
